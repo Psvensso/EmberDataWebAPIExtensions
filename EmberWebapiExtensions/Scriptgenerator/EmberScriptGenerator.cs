3785cf8661183e8568b3403791ff20086c5f03e6
Psvensso <per.svensson.se@gmail.com>
2014-03-02 15:01:05 +0100

    Added missing files to git

---
+++ EmberWebapiExtensions/Scriptgenerator/EmberScriptGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace EmberWebapiExtensions
+{
+    public class EmberScriptGenerator : IScriptGenerator
+    {
+        public EmberScriptGenerator()
+        {
+        }
+        public string GenerateModelScript()
+        {
+            var model = ModelGenerator.Instance.GenerateModel();
+            var template = new EmberWebapiExtensions.Templates.EmberModelTemplate(model);
+            return template.TransformText();
+        }
+        
+    }
+}
---
+++ EmberWebapiExtensions/Templates/EmberModelTemplateParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace EmberWebapiExtensions.Templates
+{
+    public partial class EmberModelTemplate
+    {
+        public EmberModel model { get; set; }
+        public string appNamespace { get; private set; }
+        public EmberModelTemplate(EmberModel Model)
+        {
+            model = Model;
+            var appName = WebConfigurationManager.AppSettings["EmberModel:AppNamespace"];
+            appNamespace = appName == null ? "App" : appName.ToString();
+        }
+    }
+}
